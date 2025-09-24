e44d825e0dbaf7e926e0c07d61fb16d6163c7936
parksangryong <ppsr3478@naver.com>
2025-09-24 15:41:28 +0900

    appIcon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,6 +2,8 @@ import { colors } from "@/constants";
 import { Tabs } from "expo-router";
 import React from "react";
 
+import Ionicons from "@expo/vector-icons/Ionicons";
+
 export default function TabLayout() {
   return (
     <Tabs
@@ -14,18 +16,39 @@ export default function TabLayout() {
         name="index"
         options={{
           title: "홈",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "home-sharp" : "home-outline"}
+              color={color}
+              size={24}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="my"
         options={{
           title: "내 프로필",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "person-circle" : "person-circle-outline"}
+              color={color}
+              size={24}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="setting"
         options={{
           title: "설정",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "settings" : "settings-outline"}
+              color={color}
+              size={24}
+            />
+          ),
         }}
       />
     </Tabs>
